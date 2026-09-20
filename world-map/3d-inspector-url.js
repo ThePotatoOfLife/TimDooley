47b89679946518c48e1fb8548d8538b60bdbbcbf
Tim Dooley <thepotatooflife@hotmail.com>
2026-09-20 13:35:23 +0200

    Add Axis Inspector node types

--- world-map/3d-inspector-url.js
+++ world-map/3d-inspector-url.js
@@ -1,4 +1,4 @@
-const INSPECTOR_TYPES = Object.freeze(['country','subdivision','place','evidence','evidence-record','project-case','spatial-overlay']);
+const INSPECTOR_TYPES = Object.freeze(['country','subdivision','place','evidence','evidence-record','project-case','spatial-overlay','axis','axis-depth']);
 const TYPE_RANK = new Map(INSPECTOR_TYPES.map((type, index) => [type, index]));
 
 function normalizePathNodes(nodes = []) {
