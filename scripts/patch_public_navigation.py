8388ce6b8bfc3cd98593bf77e2a07520a753227a
Tim Dooley <thepotatooflife@hotmail.com>
2026-09-26 18:56:43 +0200

    Elevator: refresh runtime-hardening assets

--- scripts/patch_public_navigation.py
+++ scripts/patch_public_navigation.py
@@ -178,8 +178,8 @@ def inject_site_elevator(text: str, page: Path) -> str:
         return text
 
     prefix = _relative_asset_prefix(page)
-    css = f'<link rel="stylesheet" href="{prefix}app/site-elevator.css?v=20260926j">'
-    js = f'<script src="{prefix}app/site-elevator.js?v=20260926j" defer></script>'
+    css = f'<link rel="stylesheet" href="{prefix}app/site-elevator.css?v=20260926k">'
+    js = f'<script src="{prefix}app/site-elevator.js?v=20260926k" defer></script>'
     text = re.sub(r"</head\s*>", css + "</head>", text, count=1, flags=re.I)
     text = re.sub(r"</body\s*>", js + "</body>", text, count=1, flags=re.I)
     return text
